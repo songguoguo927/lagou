337d62eeefe294f525ad97467bb26810bd0d821b
songguoguo927 <1351443467@qq.com>
2019-09-05 16:20:30 +0800

    项目结构重新整理

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,23 @@ const gulp = require('gulp');
 
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
-const concat = require("gulp-concat")
-//gulp sass 执行编译
+// const concat = require("gulp-concat")
+const rename = require('gulp-rename');
+const minifyHtml = require('gulp-minify-html')
+
+//css文件的处理
+//gulp sass 执行编译-压缩-重命名
 gulp.task('sass', function () {
-    gulp.src('./style/index.scss')
+    gulp.src('./src/style/scss/index.scss')
         .pipe(sass({outputStyle:'compressed'}).on('error',sass.logError))
-        .pipe(gulp.dest('public/css'));
+        .pipe(rename('index.min.css'))
+        .pipe(gulp.dest('dist/public/css'));
 });
+
+//html 文件的处理
+gulp.task('html',function(){
+    gulp.src('./*.html')
+})
 //gulp watch执行监听
 // gulp.task('sass:watch', function() {
 //     gulp.watch('./style/*.scss', ['sass']);
