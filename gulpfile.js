10feb2a149291bf026e7517ec1d00cbd845947dc
songguoguo927 <1351443467@qq.com>
2019-09-05 14:17:03 +0800

    css file concat

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,20 @@ const gulp = require('gulp');
 
 
 const sass = require('gulp-sass');
-
+sass.compiler = require('node-sass');
+const concat = require("gulp-concat")
 //gulp sass 执行编译
 gulp.task('sass', function () {
-    gulp.src('.style/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./style'));
+    gulp.src('./style/*.scss')
+        .pipe(sass({outputStyle:'compressed'}).on('error',sass.logError))
+        .pipe(gulp.dest('./style/css'));
 });
 //gulp watch执行监听
-gulp.task('watch', function() {
+gulp.task('sass:watch', function() {
     gulp.watch('./style/*.scss', ['sass']);
 });
+gulp.task('concat',function(){
+    gulp.src('./style/css/*.css')
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest('public/css'))
+})
